1557cdd3f9c68b7eef12ac755166bd0674435f33
Andre63359 <salomaoandre383@gmail.com>
2026-06-10 14:03:55 -0300

    chore(config): atualiza routes.config e main

---
+++ gerenciador-tarefas/src/Config/routes.config.js
@@ -0,0 +1,15 @@
+import TarefasPage from "../Pages/Tarefas/TarefasPage.jsx";
+import CalendarioPage from "../Pages/Calendario/CalendarioPage.jsx";
+
+const rotasArea2 = [
+  {
+    path: "/tarefas",
+    element: <TarefasPage />,
+  },
+  {
+    path: "/calendario",
+    element: <CalendarioPage />,
+  },
+];
+
+export default rotasArea2;
